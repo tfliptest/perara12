0b6011f4d8e89f66f166f87ca467dc5fd2e06f7c
Per-Arne Andersen <perara12@gmail.com>
2015-07-24 16:33:56 +0200

    Oblig 1

---
+++ src/Oblig1.java
@@ -0,0 +1,19 @@
+/**
+ * Created by perar on 24.07.2015.
+ */
+public class Oblig1 {
+
+    public static void main(String[] args){
+
+        System.out.println("Dette er min første linje");
+        System.out.println("En annen linje");
+        System.out.print("Denne linjen vil ikke avslutte slik at jeg må gjøre det selv\n");
+        System.out.println("Wollaaah!");
+
+
+
+
+    }
+
+
+}
