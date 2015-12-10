3a69de4c856014fa0af5958df7e066cca80e53f8
Morten Goodwin <morten.goodwin@uia.no>
2015-12-10 15:30:26 +0100

    Added files on / to see if it helps build

---
+++ Oblig1.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+
+public class Oblig1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dette er mitt første program");
+		System.out.println("Dette er mitt");
+		System.out.println("første program");
+		JOptionPane.showMessageDialog(null, "Dette er mitt første program");
+		JOptionPane.showInputDialog(null,"Hva heter du?");
+		System.out.println("Dette skal komme til skjermen");
+		JOptionPane.showMessageDialog(null, "Dette skal komme som dialogbokser");
+		System.out.println("En ny linje som kommer til skjermen");
+		System.out.println("En siste linje som kommer til skjermen");
+		System.out.println("En aller siste linje til skjermen");
+		
+	}
+
+}
